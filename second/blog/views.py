8ad66be91abc1c1990c0c5a77d001acfd56dd769
young <youngerous@gmail.com>
2019-02-11 23:34:57 +0900

    add pagination and faker

--- second/blog/views.py
+++ second/blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
+from faker import Faker
 
 # Create your views here.
 def home(request):
     blogs = Blog.objects # 쿼리셋
-    return render(request, 'home.html', {'blogs':blogs})
+    # 모든 블로그 글을 대상으로
+    blog_list = Blog.objects.all()
+    # 블로그 객체 세 개를 한 페이지로 자르기
+    paginator = Paginator(blog_list, 3)
+    # request된 페이지가 무엇인지 알아내고
+    page = request.GET.get('page')
+    # request된 페이지를 얻어온 뒤 return 
+    posts = paginator.get_page(page)
+    return render(request, 'home.html', {'blogs':blogs, 'posts':posts})
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
